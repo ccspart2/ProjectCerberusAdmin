99f80bf933da2d6ff673512e77e64999f82eb968
Charlie Castro <ccspart2@gmail.com>
2023-11-28 13:01:58 -0400

    Initial commit with Material 3 Implementation

---
+++ app/src/main/java/com/ccspart2/projectcerberusadmincompose/ui/theme/Type.kt
@@ -0,0 +1,71 @@
+package com.ccspart2.projectcerberusadmincompose.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    displayMedium = TextStyle(
+        fontSize = 48.sp,
+        lineHeight = 56.sp,
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight(400),
+    ),
+    displaySmall = TextStyle(
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight(400),
+    ),
+    headlineLarge = TextStyle(
+        fontSize = 34.sp,
+        lineHeight = 42.sp,
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight(400),
+        letterSpacing = 0.25.sp,
+    ),
+    headlineSmall = TextStyle(
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight(400),
+    ),
+    titleLarge = TextStyle(
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight(400),
+    ),
+    titleMedium = TextStyle(
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight(500),
+    ),
+    titleSmall = TextStyle(
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight(500),
+    ),
+    bodyLarge = TextStyle(
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight(400),
+    ),
+    bodyMedium = TextStyle(
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight(400),
+    ),
+    bodySmall = TextStyle(
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight(400),
+    ),
+)
