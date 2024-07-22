932a69f0533fe7bf7ac744f2ab38b3ada993512e
charlie.castro <ccspart2@gmail.com>
2024-07-22 11:54:50 -0400

    Made changes to the employee data class to include a mock function for previews and a Sorting function to return a sorted employee list by first and last name.

--- app/src/main/java/com/ccspart2/projectcerberusadmincompose/data/model/Employee.kt
+++ app/src/main/java/com/ccspart2/projectcerberusadmincompose/data/model/Employee.kt
@@ -7,4 +7,20 @@ data class Employee(
     val phoneNumber: String = "",
     val email: String = "",
     val isAdmin: Boolean = false
-)
+) {
+    companion object {
+        fun sortEmployeesByName(employees: List<Employee>): List<Employee> {
+            return employees.sortedWith(compareBy<Employee> { it.firstName }.thenBy { it.lastName })
+        }
+        fun mock(): Employee {
+            return Employee(
+                id = "12233445233525",
+                firstName = "Charlie",
+                lastName = "Castro",
+                phoneNumber = "7875091818",
+                email = "ccspart2@gmail.com",
+                isAdmin = true
+            )
+        }
+    }
+}
